16fcaca7a4413ead4d57c83a069a73db6f3f0a41
John Martin Lindseth <john.martin.lindseth@nav.no>
2023-08-22 12:02:22 +0200

    Remove deprecated field from Tilgang class

--- src/main/kotlin/no/nav/syfo/tilgang/Tilgang.kt
+++ src/main/kotlin/no/nav/syfo/tilgang/Tilgang.kt
@@ -2,7 +2,5 @@ package no.nav.syfo.tilgang
 
 data class Tilgang(
     val erGodkjent: Boolean = false,
-    @Deprecated("Byttet navn til erGodkjent, beholdt for tilbakekompabilitet hos konsumenter")
-    val harTilgang: Boolean = erGodkjent,
     val erAvslatt: Boolean = !erGodkjent
 )
