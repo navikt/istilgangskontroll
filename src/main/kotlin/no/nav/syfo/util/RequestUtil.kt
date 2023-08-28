84a0cce53fdb75eff69cc351ff63b70180321cdd
John Martin Lindseth <john.martin.lindseth@nav.no>
2023-08-28 15:15:24 +0200

    Add pdl client (#23)

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -7,5 +7,8 @@ const val NAV_PERSONIDENT_HEADER = "nav-personident"
 const val NAV_CONSUMER_ID_HEADER = "Nav-Consumer-Id"
 const val NAV_CONSUMER_APP_ID = "istilgangskontroll"
 
+const val TEMA_HEADER = "Tema"
+const val ALLE_TEMA_HEADERVERDI = "GEN"
+
 fun bearerHeader(token: String) = "Bearer $token"
 fun callIdArgument(callId: String) = StructuredArguments.keyValue("callId", callId)!!
