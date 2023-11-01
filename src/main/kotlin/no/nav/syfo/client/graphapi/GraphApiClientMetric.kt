7aba68b3bc6808f272284821343dcb019bee3793
geir-waagboe <geir.arne.waagbo@nav.no>
2023-11-01 09:53:18 +0100

    Fix metric endpoint

--- src/main/kotlin/no/nav/syfo/client/graphapi/GraphApiClientMetric.kt
+++ src/main/kotlin/no/nav/syfo/client/graphapi/GraphApiClientMetric.kt
@@ -4,7 +4,7 @@ import io.micrometer.core.instrument.Counter
 import no.nav.syfo.application.metric.METRICS_NS
 import no.nav.syfo.application.metric.METRICS_REGISTRY
 
-const val CALL_GRAPHAPI_USER_GROUPS_PERSON_BASE = "${METRICS_NS}call_graphapi_user_groups"
+const val CALL_GRAPHAPI_USER_GROUPS_PERSON_BASE = "${METRICS_NS}_call_graphapi_user_groups"
 const val CALL_GRAPHAPI_USER_GROUPS_PERSON_SUCCESS = "${CALL_GRAPHAPI_USER_GROUPS_PERSON_BASE}_success_count"
 const val CALL_GRAPHAPI_USER_GROUPS_PERSON_FAIL = "${CALL_GRAPHAPI_USER_GROUPS_PERSON_BASE}_fail_count"
 
