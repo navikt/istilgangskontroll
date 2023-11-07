ef1403f05ab09d551e329e62ae197e0932941391
Anders Rognstad <anders.rognstad@nav.no>
2023-11-07 19:55:42 +0100

    IS-1763: Audit log brukere endpoint. Moved auditlogging to checkTilgang in service.

--- src/main/kotlin/no/nav/syfo/tilgang/TilgangApi.kt
+++ src/main/kotlin/no/nav/syfo/tilgang/TilgangApi.kt
@@ -5,10 +5,8 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import no.nav.syfo.application.api.auth.getNAVIdent
 import no.nav.syfo.application.api.auth.isMissingNAVIdent
 import no.nav.syfo.application.exception.ForbiddenAccessSystemConsumer
-import no.nav.syfo.audit.*
 import no.nav.syfo.client.azuread.PreAuthorizedApp
 import no.nav.syfo.util.*
 
@@ -26,7 +24,7 @@ fun Route.registerTilgangApi(
             val token = call.getBearerHeader()
                 ?: throw IllegalArgumentException("Failed to check syfo tilgang for veileder. No Authorization header supplied")
             if (token.isMissingNAVIdent()) {
-                throw IllegalArgumentException("Failed to check enhetstilgang for veileder. No NAV ident in token")
+                throw IllegalArgumentException("Failed to check syfo tilgang for veileder. No NAV ident in token")
             }
 
             val tilgang = tilgangService.checkTilgangToSyfo(
@@ -85,28 +83,17 @@ fun Route.registerTilgangApi(
             val requestedPersonIdent = call.getPersonidentHeader()
                 ?: throw IllegalArgumentException("Did not find a PersonIdent in request headers")
             val token = call.getBearerHeader()
-                ?: throw IllegalArgumentException("Failed to check syfo tilgang for veileder. No Authorization header supplied")
+                ?: throw IllegalArgumentException("Failed to check tilgang to person for veileder. No Authorization header supplied")
             if (token.isMissingNAVIdent()) {
-                throw IllegalArgumentException("Failed to check enhetstilgang for veileder. No NAV ident in token")
+                throw IllegalArgumentException("Failed to check tilgang to person for veileder. No NAV ident in token")
             }
-            val veilederIdent = token.getNAVIdent()
-            val consumerClientId = call.getConsumerClientId() ?: ""
-            val appName = preAuthorizedApps.find { it.clientId == consumerClientId }?.getAppnavn() ?: throw IllegalArgumentException("Failed to check syfo tilgang for veileder. No consumer clientId was found")
+            val appName = call.getAppname(preAuthorizedApps) ?: throw IllegalArgumentException("Failed to check tilgang to person for veileder. No consumer clientId was found")
 
             val tilgang = tilgangService.checkTilgangToPerson(
                 token = token,
                 personident = requestedPersonIdent,
                 callId = callId,
-            )
-
-            auditLog(
-                CEF(
-                    suid = veilederIdent,
-                    duid = requestedPersonIdent.value,
-                    event = AuditLogEvent.Access,
-                    permit = tilgang.erGodkjent,
-                    appName = appName,
-                )
+                appName = appName,
             )
 
             if (tilgang.erGodkjent) {
@@ -122,16 +109,18 @@ fun Route.registerTilgangApi(
         post("/navident/brukere") {
             val callId = call.getCallId()
             val token = call.getBearerHeader()
-                ?: throw IllegalArgumentException("Failed to check syfo tilgang for veileder. No Authorization header supplied")
+                ?: throw IllegalArgumentException("Failed to check tilgang to brukere for veileder. No Authorization header supplied")
             if (token.isMissingNAVIdent()) {
-                throw IllegalArgumentException("Failed to check enhetstilgang for veileder. No NAV ident in token")
+                throw IllegalArgumentException("Failed to check tilgang to brukere for veileder. No NAV ident in token")
             }
+            val appName = call.getAppname(preAuthorizedApps) ?: throw IllegalArgumentException("Failed to check tilgang to brukere for veileder. No consumer clientId was found")
             val personidenter = call.receive<List<String>>()
 
             val personidenterVeilederHasAccessTo = tilgangService.filterIdenterByVeilederAccess(
                 callId = callId,
                 token = token,
                 personidenter = personidenter,
+                appName = appName,
             )
 
             call.respond(HttpStatusCode.OK, personidenterVeilederHasAccessTo)
@@ -157,4 +146,11 @@ fun Route.registerTilgangApi(
             call.respond(HttpStatusCode.OK)
         }
     }
+}
+
+private fun ApplicationCall.getAppname(
+    preAuthorizedApps: List<PreAuthorizedApp>
+): String? {
+    val consumerClientId = this.getConsumerClientId() ?: ""
+    return preAuthorizedApps.find { it.clientId == consumerClientId }?.getAppnavn()
 }
