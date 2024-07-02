faece90ecd336d0f94bb2b6b8b7fd2f6053a2e92
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-07-02 11:55:48 +0200

    IS-2335: Flatten versions dependabot (#90)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,17 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 group = "no.nav.syfo"
 version = "0.0.1"
 
-object Versions {
-    const val jacksonDataType = "2.16.1"
-    const val kluent = "1.73"
-    const val ktor = "2.3.8"
-    const val logback = "1.4.14"
-    const val logbackSyslog4jVersion = "1.0.0"
-    const val logstashEncoder = "7.4"
-    const val micrometerRegistry = "1.12.2"
-    const val mockk = "1.13.9"
-    const val nimbusJoseJwt = "9.37.3"
-    const val spek = "2.0.19"
-    const val jedis = "5.1.2"
-}
+val jacksonDataType = "2.16.1"
+val kluent = "1.73"
+val ktor = "2.3.8"
+val logback = "1.4.14"
+val logbackSyslog4jVersion = "1.0.0"
+val logstashEncoder = "7.4"
+val micrometerRegistry = "1.12.2"
+val mockk = "1.13.9"
+val nimbusJoseJwt = "9.37.3"
+val spek = "2.0.19"
+val jedis = "5.1.2"
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -32,38 +30,38 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    implementation("io.ktor:ktor-client-apache:${Versions.ktor}")
-    implementation("io.ktor:ktor-client-content-negotiation:${Versions.ktor}")
-    implementation("io.ktor:ktor-serialization-jackson:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-auth-jwt:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-call-id:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-content-negotiation:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
-    implementation("io.ktor:ktor-server-status-pages:${Versions.ktor}")
+    implementation("io.ktor:ktor-client-apache:$ktor")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor")
+    implementation("io.ktor:ktor-serialization-jackson:$ktor")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktor")
+    implementation("io.ktor:ktor-server-call-id:$ktor")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor")
+    implementation("io.ktor:ktor-server-netty:$ktor")
+    implementation("io.ktor:ktor-server-status-pages:$ktor")
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:${Versions.logback}")
-    implementation("net.logstash.logback:logstash-logback-encoder:${Versions.logstashEncoder}")
-    implementation("com.papertrailapp:logback-syslog4j:${Versions.logbackSyslog4jVersion}")
+    implementation("ch.qos.logback:logback-classic:$logback")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoder")
+    implementation("com.papertrailapp:logback-syslog4j:$logbackSyslog4jVersion")
 
     // Metrics and Prometheus
-    implementation("io.ktor:ktor-server-metrics-micrometer:${Versions.ktor}")
-    implementation("io.micrometer:micrometer-registry-prometheus:${Versions.micrometerRegistry}")
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktor")
+    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerRegistry")
 
     // Cache
-    implementation("redis.clients:jedis:${Versions.jedis}")
+    implementation("redis.clients:jedis:$jedis")
 
     // (De-)serialization
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jacksonDataType}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonDataType")
 
     // Tests
-    testImplementation("io.ktor:ktor-server-tests:${Versions.ktor}")
-    testImplementation("io.mockk:mockk:${Versions.mockk}")
-    testImplementation("io.ktor:ktor-client-mock:${Versions.ktor}")
-    testImplementation("com.nimbusds:nimbus-jose-jwt:${Versions.nimbusJoseJwt}")
-    testImplementation("org.amshove.kluent:kluent:${Versions.kluent}")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}")
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:${Versions.spek}")
+    testImplementation("io.ktor:ktor-server-tests:$ktor")
+    testImplementation("io.mockk:mockk:$mockk")
+    testImplementation("io.ktor:ktor-client-mock:$ktor")
+    testImplementation("com.nimbusds:nimbus-jose-jwt:$nimbusJoseJwt")
+    testImplementation("org.amshove.kluent:kluent:$kluent")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spek")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spek")
 }
 
 kotlin {
