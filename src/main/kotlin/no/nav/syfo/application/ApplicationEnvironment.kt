48a3e208bdd548ef3379323c2f82d6f1707ca7a2
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2024-11-18 15:59:44 +0100

    IS-2765: Bruke felles Aiven-cache (#113)

--- src/main/kotlin/no/nav/syfo/application/ApplicationEnvironment.kt
+++ src/main/kotlin/no/nav/syfo/application/ApplicationEnvironment.kt
@@ -2,9 +2,10 @@ package no.nav.syfo.application
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.server.application.*
-import no.nav.syfo.cache.RedisEnvironment
+import no.nav.syfo.cache.RedisConfig
 import no.nav.syfo.client.azuread.AzureEnvironment
 import no.nav.syfo.util.configuredJacksonMapper
+import java.net.URI
 
 data class Environment(
 
@@ -15,11 +16,11 @@ data class Environment(
         appWellKnownUrl = getEnvVar("AZURE_APP_WELL_KNOWN_URL"),
         openidConfigTokenEndpoint = getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
     ),
-
-    val redis: RedisEnvironment = RedisEnvironment(
-        host = getEnvVar("REDIS_HOST"),
-        port = getEnvVar("REDIS_PORT", "6379").toInt(),
-        secret = getEnvVar("REDIS_PASSWORD"),
+    val redisConfig: RedisConfig = RedisConfig(
+        redisUri = URI(getEnvVar("REDIS_URI_CACHE")),
+        redisDB = 0, // se https://github.com/navikt/istilgangskontroll/blob/master/README.md
+        redisUsername = getEnvVar("REDIS_USERNAME_CACHE"),
+        redisPassword = getEnvVar("REDIS_PASSWORD_CACHE"),
     ),
 
     val kode6Id: String = getEnvVar("ROLE_KODE6_ID"),
