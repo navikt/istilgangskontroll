97bdb49cdb299c254775aa581834ae19a37925f7
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2026-08-06 12:31:03 +0200

    Change Kotlin JVM plugin version
    Updated Kotlin JVM plugin version from 2.4.10 to 2.3.21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val jedis = "5.2.0"
 val microsoftGraphVersion = "6.67.0"
 
 plugins {
-    kotlin("jvm") version "2.4.10"
+    kotlin("jvm") version "2.3.21"
     id("com.gradleup.shadow") version "8.3.8"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.adarshr.test-logger") version "4.0.0"
